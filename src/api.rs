d3516febe3b0f42157ecf6ecf004f16dcedea45e
zraza-xlnx <zraza@amd.com>
2026-08-17 19:57:58 +0100

    Revert "forge: derive patchset slug from "repo url" not "pr url""

--- src/api.rs
+++ src/api.rs
@@ -1206,7 +1206,7 @@ async fn forge_webhook(
     let commit_range = format!("{}..{}", metadata.base_sha, metadata.head_sha);
     let placeholder_id = format!("mr-{}-{}", metadata.pr_number, commit_range);
 
-    let slug = metadata.repo_url.as_ref().map(|url| {
+    let slug = metadata.pr_url.as_ref().map(|url| {
         let repo = crate::forge::extract_repo_name_from_url(url);
         format!("{}-{}", repo, metadata.pr_number)
     });
